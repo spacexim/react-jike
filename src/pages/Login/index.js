6c52471fd2df42128e183bb58dc3c71c4b989c9a
Yao Zu <1796381757@qq.com>
2025-01-14 01:08:43 +0800

    添加登录页面样式和功能实现

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,47 @@
+import "./index.scss";
+import { Card, Form, Input, Button } from "antd";
+import logo from "@/assets/logo.png";
+
 const Login = () => {
-    return <div>this is Login</div>
-}
+  // 点击登录按钮时触发 参数values即是表单输入数据
+  const onFinish = (formValue) => {
+    console.log(formValue);
+  };
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger={["onBlur"]}>
+          <Form.Item
+            name="mobile"
+            rules={[
+              { required: true, message: "请输入手机号" },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: "手机号码格式不对",
+              },
+            ]}
+          >
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+
+          <Form.Item
+            name="code"
+            rules={[{ required: true, message: "请输入验证码" }]}
+          >
+            <Input size="large" placeholder="请输入验证码" maxLength={6} />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
+};
 
 export default Login;
